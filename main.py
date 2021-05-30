26d77ce9d7949d5f04c5b63ca6e63959598f5fca
Headclass <michalvano000@gmail.com>
2021-05-31 00:59:04 +0200

    Working 3D camera, needs near plane clipping.

--- main.py
+++ main.py
@@ -1,72 +1,20 @@
-from PIL import Image
 import numpy
 import math
+import tkinter
 
 #Window dimensions
-width = 1000
-height = 1000
+width = 800
+height = 800
 
 #An empty canvas
-img  = Image.new( mode = "RGB", size = (width, height) )
-pix=img.load()
-
-#Bresenham's algorithm for line rasterization
-def drawLine(x1, y1, x2, y2, color):
-    chan = 0
-    dx = x2 - x1
-    dy = y2 - y1
-    if (dx != 0 and dy != 0): #Controlling, if the line to be drawn is not a point or a line parallel to x or y axis.
-        if (abs(dy) > abs(dx)):
-            x1, y1 = y1,x1
-            x2, y2 = y2, x2
-            dx = x2 - x1
-            dy = y2 - y1
-            chan = 1
-        if (x2 < x1):
-            x1, x2=x2, x1
-            dx = x2 - x1
-            y1,y2 = y2,y1
-            dy = y2 - y1
-
-        k = (dy << 8) / dx
-        y = y1 << 8
-        for x in range(x1,x2+1):
-            if (chan == 0):
-                try:
-                    pix[x, int(y) >> 8] = color
-                except:
-                    return
-                y += k
-
-            else:
-                try:
-                    pix[int(y) >> 8,x] = color
-                except:
-                    return
-                y += k
-    else:
-        if (dy == 0 and dx != 0):
-            if (x2 < x1):
-                x1, x2=x2, x1
-            for x in range(x1,x2+1):
-                if y1 < 0 or y1 > height-1 or x < 0 or x > width-1:
-                    return
-                pix[x,y1] = color
-
-        if (dx == 0 and dy != 0):
-            if (y2 < y1):
-                y1,y2 = y2,y1
-            for y in range(y1, y2 + 1):
-                if x1 < 0 or x1 > height-1 or y < 0 or y > width-1:
-                    return
-                pix[x1,y] = color
+cnv = tkinter.Canvas(bg='white', width=width, height=height)
 
 #Triangle rasterization
-def drawTriangle(triangle,color):
-    drawLine(triangle[0][0],triangle[0][1],triangle[1][0],triangle[1][1],color)
-    drawLine(triangle[1][0],triangle[1][1],triangle[2][0],triangle[2][1],color)
-    drawLine(triangle[2][0],triangle[2][1],triangle[0][0],triangle[0][1],color)
-
+def drawTriangle(triangle):
+    cnv.create_line(triangle[0][0],height-triangle[0][1],triangle[1][0],height-triangle[1][1])
+    cnv.create_line(triangle[1][0],height-triangle[1][1],triangle[2][0],height-triangle[2][1])
+    cnv.create_line(triangle[2][0],height-triangle[2][1],triangle[0][0],height-triangle[0][1])
+    #cnv.create_polygon(triangle[0][0],height-triangle[0][1],triangle[1][0],height-triangle[1][1],triangle[2][0],height-triangle[2][1],fill='white',outline='black')
 
 #Adding a homogenous coordinate (w)
 def homogenous(vertex):
@@ -76,28 +24,34 @@ def homogenous(vertex):
 def transpose(vertex):
     return numpy.array([vertex]).T
 
+
 #SPACE CONVERSION
 
 #Our cube is in its model space. We want to put it onto our scene, while rotating it a bit and moving it further away from the camera.
 #model space->world space
-
-#Rotation angles
-xangle = math.radians(69)
-yangle = math.radians(39)
-zangle = math.radians(0)
-
-#Rotation matrices
-xRotationMatrix = numpy.array([[1, 0, 0, 0], [0,  math.cos(xangle), -math.sin(xangle), 0],[0, math.sin(xangle), math.cos(xangle), 0],[0,0,0,1]])
-yRotationMatrix = numpy.array([[math.cos(yangle), 0, math.sin(yangle), 0], [0, 1, 0, 0],[-math.sin(yangle), 0, math.cos(yangle), 0],[0,0,0,1]])
-zRotationMatrix = numpy.array([[math.cos(zangle), -math.sin(zangle), 0, 0], [math.sin(zangle), math.cos(zangle), 0, 0],[0, 0, 1, 0],[0,0,0,1]])
-#Translation along the negative Z axis
-TranslationMatrix = numpy.array([[1, 0, 0, 0], [0, 1, 0, 0],[0, 0, 1, -6],[0,0,0,1]])
-#Combining the transformations into one model matrix
-ModelMatrix = numpy.dot(yRotationMatrix,xRotationMatrix)
-ModelMatrix = numpy.dot(zRotationMatrix,ModelMatrix)
-ModelMatrix=numpy.dot(TranslationMatrix,ModelMatrix)
 #Applying the transformation to all of our vertexes
-def modelToWorld(vertex):
+def modelToWorld(vertex,x,y,z):
+    # Rotation angles
+    xangle = math.radians(x)
+    yangle = math.radians(y)
+    zangle = math.radians(z)
+
+    # Rotation matrices
+    xRotationMatrix = numpy.array(
+        [[1, 0, 0, 0], [0, math.cos(xangle), -math.sin(xangle), 0], [0, math.sin(xangle), math.cos(xangle), 0],
+         [0, 0, 0, 1]])
+    yRotationMatrix = numpy.array(
+        [[math.cos(yangle), 0, math.sin(yangle), 0], [0, 1, 0, 0], [-math.sin(yangle), 0, math.cos(yangle), 0],
+         [0, 0, 0, 1]])
+    zRotationMatrix = numpy.array(
+        [[math.cos(zangle), -math.sin(zangle), 0, 0], [math.sin(zangle), math.cos(zangle), 0, 0], [0, 0, 1, 0],
+         [0, 0, 0, 1]])
+    # Translation along the negative Z axis
+    TranslationMatrix = numpy.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, -6], [0, 0, 0, 1]])
+    # Combining the transformations into one model matrix
+    ModelMatrix = numpy.dot(yRotationMatrix, xRotationMatrix)
+    ModelMatrix = numpy.dot(zRotationMatrix, ModelMatrix)
+    ModelMatrix = numpy.dot(TranslationMatrix, ModelMatrix)
     return numpy.dot(ModelMatrix,vertex)
 
 
@@ -105,13 +59,41 @@ def modelToWorld(vertex):
 #We cannot move the camera itself, we need to move the world. So in order to move the camera 1 unit closer to the cube,
 #we need to move the cube closer to the camera. Remember, the camera always points to the negative Z axis.
 #world space->view space
-
 #View matrix
-ViewMatrix = numpy.array([[1, 0, 0, 0], [0, 1, 0, 0],[0, 0, 1, 3],[0,0,0,1]])
 #Applying the transformation to all of our vertexes
+xcam, ycam, zcam = 0, 0, 0
+camXangle, camYangle, camZangle = 0, 0, 0
+ViewMatrix = numpy.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+
+def updateView():
+    global ViewMatrix, xcam, ycam, zcam, camXangle, camYangle, camZangle
+
+    CamyRotationMatrix = numpy.array(
+        [[math.cos(math.radians(camYangle)), 0, math.sin(math.radians(camYangle)), 0], [0, 1, 0, 0],
+         [-math.sin(math.radians(camYangle)), 0, math.cos(math.radians(camYangle)), 0],
+         [0, 0, 0, 1]])
+    CamxRotationMatrix = numpy.array(
+        [[1, 0, 0, 0], [0, math.cos(math.radians(camXangle)), -math.sin(math.radians(camXangle)), 0],
+         [0, math.sin(math.radians(camXangle)), math.cos(math.radians(camXangle)), 0],
+         [0, 0, 0, 1]])
+    CamzRotationMatrix = numpy.array(
+        [[math.cos(math.radians(camZangle)), -math.sin(math.radians(camZangle)), 0, 0], [math.sin(math.radians(camZangle)), math.cos(math.radians(camZangle)), 0, 0], [0, 0, 1, 0],
+         [0, 0, 0, 1]])
+
+    CamTranslationMatrix = numpy.array([[1, 0, 0, 0 + xcam], [0, 1, 0, 0 + ycam], [0, 0, 1, 0 + zcam], [0, 0, 0, 1]])
+
+    ViewMatrix = numpy.dot(CamxRotationMatrix, ViewMatrix)
+    ViewMatrix = numpy.dot(CamyRotationMatrix, ViewMatrix)
+    ViewMatrix = numpy.dot(CamzRotationMatrix, ViewMatrix)
+
+    ViewMatrix = numpy.dot(CamTranslationMatrix, ViewMatrix)
+
+    xcam, ycam, zcam = 0, 0, 0
+    camXangle, camYangle, camZangle = 0, 0, 0
+
 def worldToView(vertex):
     return numpy.dot(ViewMatrix,vertex)
-
 #Now we need to apply the projection matrix to create perspective.
 #view space->clip space
 
@@ -144,7 +126,7 @@ def roundPixel(vertex):
 
 
 #Vertexes of cube triangles
-cubeMesh=[
+cubeMesh2=[
                 [-1,-1,-1],[1,-1,-1],[1,-1,1]  ,  [-1,-1,-1],[1,-1,1],[-1,-1,1], #TOP
                 [1,-1,-1],[1,1,-1],[1,1,1]     ,  [1,-1,-1],[1,1,1],[1,-1,1],    #RIGHT
                 [-1,1,-1],[-1,-1,-1],[-1,-1,1] ,  [-1,1,-1],[-1,-1,1],[-1,1,1],  #LEFT
@@ -153,28 +135,147 @@ cubeMesh=[
                 [1,-1,-1],[-1,-1,-1],[-1,1,-1]  ,  [1,-1,-1],[-1,1,-1],[1,1,-1]  #FAR
 ]
 
+cubeMesh=[
+[-2, 0, -2],[2, 0, -2],[-2, 0, 2],
+[-2, 0, 2],[2, 0, -2],[2, 0, 2],
+
+
+
+[-0.5, 0., -0.5 ],
+[-0.5, 1., -0.5 ],
+[-0.5, 0, 0.5],
+
+[-0.5, 1, -0.5],
+[-0.5, 0., 0.5],
+[-0.5, 1., 0.5],
+
+[-0.5, 0., -0.5],
+[-0.5, 1., -0.5],
+[0.5, 0., -0.5],
+
+[-0.5, 1., -0.5],
+[0.5, 0., -0.5],
+[0.5, 1., -0.5],
+
+[0.5, 0., -0.5],
+[0.5, 1., -0.5],
+[0.5, 0., 0.5],
+
+[0.5, 1., -0.5],
+[0.5, 0., 0.5],
+[0.5, 1, 0.5],
+
+[-0.5, 0., 0.5],
+[-0.5, 1., 0.5],
+[0.5, 0., 0.5],
+
+[-0.5, 1., 0.5],
+[0.5, 0., 0.5],
+[0.5, 1., 0.5],
+
+[-0.25, 0.0, 0.5],
+[0.25, 0., 0.5],
+[0.25, 0.5, 0.5],
+[-0.25, 0., 0.5],
+[0.25, 0.5, 0.5],
+[-0.25, 0.5, 0.5],
+
+
+
+[-0.5, 1., -0.5],
+[-0.5, 1., 0.5],
+[0., 2., 0.0],
+
+[0.5, 1., -0.5],
+[0.5, 1., 0.5],
+[0., 2., 0.0],
+
+[-0.5, 1., -0.5],
+[0.5, 1., -0.5],
+[0., 2., 0.0],
+
+[-0.5, 1, 0.5],
+[0.5, 1, 0.5],
+[0., 2., 0.0]
+]
 
 
 #An empty triangle
-Triangle=[[0,0],[0,0],[0,0]]
+Triangle=[[0,0,0],[0,0,0],[0,0,0]]
 
 #Colors
 colors = [(255,0,0),(255,0,0),(0,255,0),(0,255,0),(0,0,255),(0,0,255),(255,255,0),(255,255,0),(0,255,255),(0,255,255),(255,0,255),(255,0,255)]
 
 #Triangle counter
 counter=0
+cnv.pack()
+
+
 for i in range(len(cubeMesh)):
-    homogenous(cubeMesh[i])                            #Adding a homogenous coordinate
-    cubeMesh[i]=transpose(cubeMesh[i])                 #Changing a row vector to a column vector
-    cubeMesh[i]=modelToWorld(cubeMesh[i])              #Moving our model to its place on world coordinates
-    cubeMesh[i]=worldToView(cubeMesh[i])               #Moving the world relative to our camera ("moving" the camera)
-    cubeMesh[i]=viewToClip(cubeMesh[i])                #Applying projection
-    cubeMesh[i]=perspectiveDivision(cubeMesh[i])       #Dividing by W to get to normalised device coordinates
-    cubeMesh[i]=viewportTransformation(cubeMesh[i])    #Changing the normalised device coordinates to pixels on the screen
-    cubeMesh[i]=roundPixel(cubeMesh[i])                #Rounding the resulting values to nearest pixel
-    Triangle[i%3][0] = int(cubeMesh[i][0])
-    Triangle[i%3][1] = int(cubeMesh[i][1])
-    if i%3==2:
-        drawTriangle(Triangle,colors[counter%12])
-        counter+=1
-img.show()
+    homogenous(cubeMesh[i])               # Adding a homogenous coordinate
+    cubeMesh[i] = transpose(cubeMesh[i])  # Changing a row vector to a column vector
+
+
+changingMesh = cubeMesh.copy()
+j=0
+def update():
+        cnv.delete("all")
+        updateView()
+        global j
+        for i in range(len(cubeMesh)):
+            changingMesh[i]=modelToWorld(cubeMesh[i],0,0,0)              #Moving our model to its place on world coordinates
+            changingMesh[i]=worldToView(changingMesh[i])               #Moving the world relative to our camera ("moving" the camera)
+            changingMesh[i]=viewToClip(changingMesh[i])                #Applying projection
+            changingMesh[i] = perspectiveDivision(changingMesh[i])  # Dividing by W to get to normalised device coordinates
+            changingMesh[i] = viewportTransformation(changingMesh[i])  # Changing the normalised device coordinates to pixels on the screen
+            changingMesh[i] = roundPixel(changingMesh[i])  # Rounding the resulting values to nearest pixel
+            Triangle[i%3][0] = int(changingMesh[i][0])
+            Triangle[i%3][1] = int(changingMesh[i][1])
+            Triangle[i % 3][2] = int(changingMesh[i][2])
+            if i%3==2:
+                drawTriangle(Triangle)
+        j+=1
+        if j == 5*365:
+            j=0
+        cnv.after(5,update)
+
+update()
+def move(event):
+    global xcam
+    global ycam
+    global zcam
+    global camXangle
+    global camYangle
+    global camZangle
+
+    if event.char=='w':
+        zcam+=0.2
+    if event.char == 's':
+        zcam-=0.2
+    if event.char=='a':
+        xcam+=0.2
+    if event.char == 'd':
+        xcam-=0.2
+    if event.char=='q':
+        ycam-=0.2
+    if event.char == 'e':
+        ycam+=0.2
+
+    if event.char == 'i':
+        camXangle -= 4
+    if event.char == 'k':
+        camXangle += 4
+    if event.char == 'j':
+        camYangle -= 4
+    if event.char == 'l':
+        camYangle += 4
+    if event.char == 'u':
+        camZangle -= 4
+    if event.char == 'o':
+        camZangle +=4
+
+
+cnv.bind_all('<Key>', move)
+
+
+tkinter.mainloop()
